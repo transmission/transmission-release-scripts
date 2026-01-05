65744caa42fae65a5ebf631b4a49d81299691656
Mitchell Livingston <livings124@mac.com>
2026-01-04 21:53:24 -0500

    Sign QuickLookExtension.appex fully

--- build-macos.sh
+++ build-macos.sh
@@ -29,7 +29,7 @@ build_transmission() {
         security list-keychains -s "${KEYCHAIN_NAME}"
         
         codesign --force --timestamp --options runtime -v -s "${CERT_NAME}" dmg/Transmission.app/Contents/Library/QuickLook/QuickLookPlugin.qlgenerator/Contents/MacOS/QuickLookPlugin
-        codesign --force --timestamp --options runtime -v -s "${CERT_NAME}" --entitlements ./macosx/QuickLookExtension/QuickLookExtension.entitlements dmg/Transmission.app/Contents/PlugIns/QuickLook/QuickLookExtension.appex/Contents/MacOS/QuickLookExtension
+        codesign --force --timestamp --options runtime -v -s "${CERT_NAME}" --entitlements ./macosx/QuickLookExtension/QuickLookExtension.entitlements dmg/Transmission.app/Contents/PlugIns/QuickLook/QuickLookExtension.appex
         codesign --force --timestamp --options runtime -v -s "${CERT_NAME}" dmg/Transmission.app/Contents/Frameworks/Sparkle.framework
         codesign --force --timestamp --options runtime -v -s "${CERT_NAME}" dmg/Transmission.app
         
